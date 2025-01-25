b78838dc33dc7707e04bdccf96f02bd9e2f65178
Edgar <edgar1124@gmail.com>
2025-01-25 22:16:12 +0100

    Updated .py file

--- createStockList_PolygonIO.py
+++ createStockList_PolygonIO.py
@@ -70,27 +70,29 @@ i = 0
 exchange_today = exchange_list[datetime.datetime.today().day%4]
 
 print('Let"s go go go!')
-for t in client_RESTClient.list_tickers(market='stocks', exchange = exchange_today):
-    logging.info(f'/\/\/\/\/\/\/\/\/\/\/Beginning to process: {t}/\/\/\/\/\/\/\/\/\/\/')
-    
-    try:
-        active_tickers.append(t.ticker)
-        logging.info(f'Exchange: {exchange_today}, ticker: {t.ticker}, counter: {i}')
-        i+=1
-        sleep(20)
 
-        if i%100 == 0:
+try:
+    for t in client_RESTClient.list_tickers(market='stocks', exchange = exchange_today):
+        try:
+            active_tickers.append(t.ticker)
+            logging.info(f'Exchange: {exchange_today}, ticker: {t.ticker}, counter: {i}')
+            i+=1
+            sleep(20)
+
+            if i%100 == 0:
+                        
+                try:
+                    emailTool.SendEmail(f'Processed: {i}')
                     
-            try:
-                emailTool.SendEmail(f'Exchange: {exchange_today}, ticker: {t.ticker}, counter: {i}')
-                
-            except Exception as e:
-                logging.info(f'/\/\/\/\/\/\/\/\/\/\/Main exception: {e}/\/\/\/\/\/\/\/\/\/\/')
+                except Exception as e:
+                    logging.info(f'/\/\/\/\/\/\/\/\/\/\/Email exception: {e}/\/\/\/\/\/\/\/\/\/\/')
 
 
-    except Exception as e:
-        logging.info(f'/\/\/\/\/\/\/\/\/\/\/Main exception: {e}/\/\/\/\/\/\/\/\/\/\/')
+        except Exception as e:
+            logging.info(f'/\/\/\/\/\/\/\/\/\/\/Main exception: {e}/\/\/\/\/\/\/\/\/\/\/')
 
+except Exception as E:
+    logging.info(f'/\/\/\/\/\/\/\/\/\/\/Main exception: {e}/\/\/\/\/\/\/\/\/\/\/')
 
 logging.info('/\/\/\/\/\/\/\/\/\/\/Stock list compiled/\/\/\/\/\/\/\/\/\/\/')
 file_name = exchange_today + '_StockList_' + i
